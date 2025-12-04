207fc267aedba62cf1d79d61bd98a85ebcf3cc6b
Krishnakanthpps <krishnakanth.pps@gmail.com>
2025-12-04 09:44:07 +0530

    Configure CORS to allow frontend domain

--- server/server.js
+++ server/server.js
@@ -7,7 +7,23 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+const allowedOrigins = [
+    'http://localhost:5173',
+    'http://localhost:5000',
+    'https://divyakaryaclient.vercel.app',
+    'https://divyakaryaserver.vercel.app'
+];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true
+}));
 app.use(express.json());
 
 const { User, Pandit, Booking } = require('./models');
